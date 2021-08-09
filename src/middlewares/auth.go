fd8b1c34e8b645899c404e4eec71dade3c7c63c2
Gustavo Velazquez <Gustavo.Velazquez@encora.com>
2021-08-09 11:46:53 -0500

    Get authorization cookie

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -1,11 +1,14 @@
 package middlewares
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		log.Println(c.Cookie("authorization"))
 		// c.AbortWithStatus(403)
 		c.Next()
 	}
