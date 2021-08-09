3a7a87b24d2461888507d78228b13b90cebbeb10
Gustavo Velazquez <Gustavo.Velazquez@encora.com>
2021-08-09 09:24:31 -0500

    Auth middleware 403 error

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -1,14 +1,12 @@
 package middlewares
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Println("Auth middleware")
+		// c.AbortWithStatus(403)
 		c.Next()
 	}
 }
